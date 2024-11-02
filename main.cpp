4ef7d242c5e7f20094e4c691241aa6b5cbb1e52d
myc <1351579650@qq.com>
2024-11-02 16:54:38 +0800

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,86 @@
+#include <Windows.h>
+#include <crtdbg.h>
+#include <exception>
+
+static int g_width  = 800;
+static int g_height = 600;
+
+HWND createWindow(const wchar_t* name, int width, int height);
+// Call after creating DirectX resources
+void updateWindow(HWND hWnd);
+LRESULT CALLBACK wndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR, int)
+{
+#ifdef _DEBUG
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    auto hWnd = createWindow(L"LearnDirectX12", g_width, g_height);
+    // TODO: Create DX12 Resources
+    updateWindow(hWnd);
+
+    MSG msg = {};
+    while (msg.message != WM_QUIT)
+    {
+        if (PeekMessageW(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    
+        // TODO: Render here
+    }
+}
+
+HWND createWindow(const wchar_t* name, int width, int height)
+{
+    auto hInstance = GetModuleHandleW(nullptr);
+
+    WNDCLASSEXW wc = {};
+    wc.cbSize        = sizeof(wc);
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = name;
+    wc.style         = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc   = wndProc;
+    if (RegisterClassExW(&wc) == 0)
+        throw std::exception("RegisterClassExW Error");
+
+    // Adjust real size, include title bar
+    RECT rect = { 0, 0, width, height };
+    AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+    width  = rect.right - rect.left;
+    height = rect.bottom - rect.top;
+
+    auto hWnd = CreateWindowW(
+        name, nullptr,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, width, height,
+        nullptr, nullptr, hInstance, nullptr
+    );
+    if (hWnd == nullptr)
+        throw std::exception("CreateWindowW Error"); 
+
+    return hWnd;
+}
+
+void updateWindow(HWND hWnd)
+{
+    ShowWindow(hWnd, SW_SHOW);
+    UpdateWindow(hWnd);
+}
+
+LRESULT CALLBACK wndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_MENUCHAR:
+        // Disable beep when Alt + Enter
+        return MAKELRESULT(0, MNC_CLOSE);
+    }
+    return DefWindowProcW(hWnd, msg, wParam, lParam);
+}
