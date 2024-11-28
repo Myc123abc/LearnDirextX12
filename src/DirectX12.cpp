2f239b291e711e98b0326f519d48e97e7bea9138
myc <1351579650@qq.com>
2024-11-28 22:36:03 +0800

    msaa wireframe!

--- src/DirectX12.cpp
+++ src/DirectX12.cpp
@@ -496,8 +496,10 @@ void DirectX12::createPipeline()
         m_ps->GetBufferSize()
     };
     psoDesc.RasterizerState       = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
-    // psoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME;
     // psoDesc.RasterizerState.CullMode = D3D12_CULL_MODE_FRONT;
+    psoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME;
+    psoDesc.RasterizerState.AntialiasedLineEnable = TRUE;
+    psoDesc.RasterizerState.MultisampleEnable = TRUE;
     psoDesc.BlendState            = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
     psoDesc.DepthStencilState     = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
     psoDesc.SampleMask            = UINT_MAX;
